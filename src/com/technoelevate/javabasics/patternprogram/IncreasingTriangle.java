b27f1ed7f8864097fcc50fef4436e2ef54852cac
PARAG BANDEKAR <paragb1999@gmail.com>
2021-11-01 14:44:13 +0530

    pattern program

---
+++ src/com/technoelevate/javabasics/patternprogram/IncreasingTriangle.java
@@ -0,0 +1,18 @@
+package com.technoelevate.javabasics.patternprogram;
+
+
+public class IncreasingTriangle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n=5;
+		for(int i=1;i<=n;i++) {
+			for(int j=1;j<=i;j++) {
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
