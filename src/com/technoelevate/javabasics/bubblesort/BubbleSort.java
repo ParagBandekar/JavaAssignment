30cc7f8952df49321914f1248ad830c9a133a920
PARAG BANDEKAR <paragb1999@gmail.com>
2021-11-14 19:37:00 +0530

    assignment 28-10-21

---
+++ src/com/technoelevate/javabasics/bubblesort/BubbleSort.java
@@ -0,0 +1,22 @@
+package com.technoelevate.javabasics.bubblesort;
+
+public class BubbleSort {
+	public static void main(String[] args) {
+
+		int [] arr= {10,-25,-60,45,62};
+		
+		for(int i=0;i<=arr.length-1;i++) {
+			for(int j=i+1;j<arr.length ;j++) {
+				
+				if(arr[i]>arr[j]){
+					int temp=arr[i];
+					arr[i]=arr[j];
+					arr[j]=temp;
+				}
+				}
+			}
+		for(int k=0;k<arr.length;k++) {
+			System.out.print(arr[k]+" ");
+		}
+	}
+	 }
